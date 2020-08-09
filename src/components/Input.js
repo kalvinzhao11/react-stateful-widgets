da829b21470950ef79814c32715e22ed62756957
Kalvin Zhao <kalvinzhao@Kalvins-MacBook-Pro.local>
2020-08-09 15:53:59 -0400

    added input

--- src/components/Input.js
+++ src/components/Input.js
@@ -30,10 +30,11 @@ STEP 5:
   What should we do inside this click handler?
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Input() {
   /* STEP 1 */
+  const [inputValue, setInputValue] = useState('')
 
   const changeInput = evt => {
     // When the input changes, its whole value can be found inside the event object.
@@ -41,21 +42,23 @@ export default function Input() {
     const { value } = evt.target;
 
     /* STEP 4 */
+    setInputValue(value)
   };
   const reset = () => {
     /* STEP 5 */
+    setInputValue('')
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: inputValue.length > 9 ? 'royalblue': 'black', /* STEP 2 */
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div style={style}></div> {/* STEP 3 */}
+      <div style={style}>{inputValue}</div> {/* STEP 3 */}
       <div>
         <input type='text' onChange={changeInput} />
         <button onClick={reset}>Reset</button>
